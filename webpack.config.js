41733dbb20d210e717c7d128adec53ecec98af96
Pierre Martin <pierre@occitech.fr>
2018-11-07 23:42:56 +0100

    CS format

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    app: './src/index.js'
+    app: "./src/index.js"
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist',
-    host: '0.0.0.0',
+    contentBase: "./dist",
+    host: "0.0.0.0",
     port: 1234,
     overlay: {
       warnings: true,
@@ -20,22 +20,22 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
       inject: false,
-      template: require('html-webpack-template'),
+      template: require("html-webpack-template"),
       mobile: true,
 
-      title: 'Solid words',
-      lang: 'en-US',
-      appMountId: 'app'
+      title: "Solid words",
+      lang: "en-US",
+      appMountId: "app"
     }),
     new WriteFilePlugin(),
-    new CopyWebpackPlugin(['./public/'])
+    new CopyWebpackPlugin(["./public/"])
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
@@ -43,11 +43,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       }
     ]
   },
   // see https://www.npmjs.com/package/@solid/react#configuring-webpack
-  externals: require('@solid/react/webpack/webpack.bundle.config').externals
+  externals: require("@solid/react/webpack/webpack.bundle.config").externals
 };
